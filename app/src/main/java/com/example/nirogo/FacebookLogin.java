ee9a8eb7fa11586d768e6315d1c6884b479d4860
mishra5047 <mishra695047@gmail.com>
2020-06-28 23:27:48 +0530

    Facebook Fix - 1

--- app/src/main/java/com/example/nirogo/FacebookLogin.java
+++ app/src/main/java/com/example/nirogo/FacebookLogin.java
@@ -1,18 +1,22 @@
 package com.example.nirogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nirogo.Activities.LoginActivity;
 import com.example.nirogo.Doctor.DetailsDoctor;
 import com.example.nirogo.HomeScreen.HomeActivity;
 import com.example.nirogo.User.DetailsUser;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -20,6 +24,11 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
@@ -32,6 +41,15 @@ public class FacebookLogin extends AppCompatActivity {
     Button back;
     ImageView image;
     TextView continueTxt;
+    private String TAG = "FB";
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,8 @@ public class FacebookLogin extends AppCompatActivity {
         setContentView(R.layout.activity_facebook_login);
 
         final String type = getIntent().getStringExtra("type");
+
+        mAuth = FirebaseAuth.getInstance();
 
         back = findViewById(R.id.backBtn);
         back.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +94,7 @@ public class FacebookLogin extends AppCompatActivity {
                 @Override
                 public void onSuccess(LoginResult loginResult) {
                     Picasso.get().load("http://graph.facebook.com/" + loginResult.getAccessToken().getUserId() + "/picture?type=square").into(image);
+                        handleFacebookAccessToken(loginResult.getAccessToken());
                 }
 
                 @Override
@@ -88,7 +109,33 @@ public class FacebookLogin extends AppCompatActivity {
             });
         }
 
-        @Override
+        private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(FacebookLogin.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    @Override
         protected void onActivityResult ( int requestCode, int resultCode, Intent data){
             super.onActivityResult(requestCode, resultCode, data);
 
