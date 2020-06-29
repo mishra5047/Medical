4c28040bfbe60c6e815aa6208633cf5773d8f28b
mishra5047 <mishra695047@gmail.com>
2020-06-29 16:52:42 +0530

    Final Fixes - 2

--- app/src/main/java/com/example/nirogo/Activities/LoginActivity.java
+++ app/src/main/java/com/example/nirogo/Activities/LoginActivity.java
@@ -391,17 +391,6 @@ public class LoginActivity extends Activity {
 
 }
 
-    public static void hideKeyboard(Activity activity) {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
-        }
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
     public static String printKeyHash(Activity context) {
         PackageInfo packageInfo;
         String key = null;
